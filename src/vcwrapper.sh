c20630b93c6ec7d7f9a282e06dd294c968f86f4f
N-Storm <N-Storm@users.noreply.github.com>
2025-02-17 10:51:19 +0300

    fix wrapper

--- src/vcwrapper.sh
+++ src/vcwrapper.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-cmd "/C C:\Progra~1\Micros~2\2022\Enterp~1\Common7\Tools\VsDevCmd.bat & $*"
+cmd "/C C:\Progra~1\Micros~2\2022\Enterp~1\Common7\Tools\VsDevCmd.bat -arch=amd64 -host_arch=amd64 & $*"
