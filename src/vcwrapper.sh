889a5e5d66199ca2f64e7ae5643f474631ca3d80
N-Storm <N-Storm@users.noreply.github.com>
2025-02-17 11:36:33 +0300

    Trying to fix paths with spaces.

--- src/vcwrapper.sh
+++ src/vcwrapper.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-cmd "/C C:\Progra~1\Micros~2\2022\Enterp~1\Common7\Tools\VsDevCmd.bat -arch=amd64 -host_arch=amd64 & $*"
+# powershell & "C:\Progra~1\Micros~2\2022\Enterp~1\Common7\Tools\Launch-VsDevShell.ps1 -Arch amd64 -HostArch amd64 & $*"
+powershell  "& 'C:\Program Files\Microsoft Visual Studio\2022\Community\Common7\Tools\Launch-VsDevShell.ps1' -Arch amd64 -HostArch amd64 & $*"
