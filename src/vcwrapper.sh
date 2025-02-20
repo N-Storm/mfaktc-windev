d5fdd05d07cc1a1516bfb238813d3d8a270a79f4
N-Storm <N-Storm@users.noreply.github.com>
2025-02-20 15:07:52 +0300

    MSVC path fix

--- src/vcwrapper.sh
+++ src/vcwrapper.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 # powershell & "C:\Progra~1\Micros~2\2022\Enterp~1\Common7\Tools\Launch-VsDevShell.ps1 -Arch amd64 -HostArch amd64 & $*"
-powershell "& 'C:\Program Files\Microsoft Visual Studio\2022\Community\Common7\Tools\Launch-VsDevShell.ps1' -Arch amd64 -HostArch amd64; $*"
+powershell "& 'C:\Program Files\Microsoft Visual Studio\2022\Enterprise\Common7\Tools\Launch-VsDevShell.ps1' -Arch amd64 -HostArch amd64; $*"
 # powershell "& 'D:\Tools\Dev\msys64\tmp\path with space\test.ps1' -Arch amd64 -HostArch amd64; $*"
