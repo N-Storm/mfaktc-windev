0835cf12f0bed02c89d57c1adc830f10dbfed1df
N-Storm <N-Storm@users.noreply.github.com>
2025-03-05 01:08:02 +0300

    0.23.2 merged

--- .github/workflows/scripts/build_helper.sh
+++ .github/workflows/scripts/build_helper.sh
@@ -1,32 +1,39 @@
 #!/bin/bash
 #
-# TODO: Write description
+# build_helper.sh
+# This script are used by the CI/CD builds with Github Actions workflow.
+# It gathers some info on software versions and saves it to file 
+# build_helper.sh.out which are later used by the actions workflow.
+# It also patches Makefiles to support building under Github Action
+# runners environment and compile kernels for every compute capability
+# device supported by the NVCC.
 #
-# SPDX-License-Identifier: GPL-3.0-or-later
-# 
+# Copyright (c) 2025 NStorm (https://github.com/N-Storm/)
 # This file is part of mfaktc.
 # Copyright (C) 2009, 2010, 2011  Oliver Weihe (o.weihe@t-online.de)
-# 
+#
 # mfaktc is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
 # (at your option) any later version.
-# 
+#
 # mfaktc is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
-#                                 
+#
 # You should have received a copy of the GNU General Public License
 # along with mfaktc.  If not, see <http://www.gnu.org/licenses/>.
+#
+# SPDX-License-Identifier: GPL-3.0-or-later
 
 if [[ -z "$1" ]]; then
   echo "Usage: $0 <CUDA version>" >&2
   exit 1
 fi
 
-# Windows may have it's sort first on PATH, so set this var to the full path 
-# to GNU sort to avoid having writing full path every time.
+# Windows may have it's sort first on PATH, this is the shortcut
+# to call GNU sort by the full path
 export GSORT='/usr/bin/sort'
 
 declare -a CUDA_VERSION
@@ -36,8 +43,10 @@ if [[ -z "${CUDA_VERSION[*]}" ]]; then
   exit 2
 fi
 
-eval $(echo -e "CUDA_VER_MAJOR=${CUDA_VERSION[0]}\nCUDA_VER_MINOR=${CUDA_VERSION[1]}" | tee $0.out)
+CUDA_VER_MAJOR=${CUDA_VERSION[0]}
+CUDA_VER_MINOR=${CUDA_VERSION[1]}
 CUDA_VER="${CUDA_VER_MAJOR}${CUDA_VER_MINOR}"
+echo -e "CUDA_VER_MAJOR=${CUDA_VER_MAJOR}\nCUDA_VER_MINOR=${CUDA_VER_MINOR}" > "$0.out"
 
 # Starting from 11.0.0 CUDA has --list-gpu-arch flag.
 # For older versions we have to grep out supported CC versions from help.
@@ -56,7 +65,7 @@ elif [ ${#CC_LIST[*]} -lt 3 ]; then
 fi
 
 echo "All supported CCs: ${CC_LIST[*]}, CC_MIN=${CC_LIST[0]}, CC_MAX=${CC_LIST[-1]}"
-echo -e "CC_LIST=\"${CC_LIST[*]}\"\nCC_MIN=${CC_LIST[0]}\nCC_MAX=${CC_LIST[-1]}" >> $0.out
+echo -e "CC_LIST=\"${CC_LIST[*]}\"\nCC_MIN=${CC_LIST[0]}\nCC_MAX=${CC_LIST[-1]}" >> "$0.out"
 
 echo 'Removing NVCCFLAGS strings with CC arch entries from the Makefile & Makefile.win and populating with discovered supported values.'
 sed -i '/^NVCCFLAGS += --generate-code arch=compute.*/d' src/Makefile.win src/Makefile
@@ -74,22 +83,23 @@ if [ $CUDA_VER -lt 120 ]; then
 fi
 
 echo 'Gathering version info on generic compiler and nvcc...'
-if [[ -x "$(which vswhere.exe)" ]]; then
+if [[ -x "$(command -v vswhere.exe)" ]]; then
   CC_VSPROD="$(vswhere -latest -products '*' -requires Microsoft.VisualStudio.Component.VC.Tools.x86.x64 -property displayName)"
-  CC_VER="${CC_VSPROD}, $(vswhere -latest -products '*' -requires Microsoft.VisualStudio.Component.VC.Tools.x86.x64 -property installationVersion)"
-elif [[ -x "$(which powershell.exe)" ]]; then
+  COMPILER_VER="${CC_VSPROD}, $(vswhere -latest -products '*' -requires Microsoft.VisualStudio.Component.VC.Tools.x86.x64 -property installationVersion)"
+elif [[ -x "$(command -v powershell.exe)" ]]; then
   CC_VSINFO="$(powershell -Command Get-VSSetupInstance)"
-  CC_VSPROD="$(echo $CC_VSINFO | grep DisplayName | cut -d':' -f2 | xargs)"
-  CC_VER="${CC_VSPROD}, $(echo $CC_VSINFO | grep InstallationVersion | cut -d':' -f2 | xargs)"
+  CC_VSPROD="$(echo "$CC_VSINFO" | grep DisplayName | cut -d':' -f2 | xargs)"
+  COMPILER_VER="${CC_VSPROD}, $(echo "$CC_VSINFO" | grep InstallationVersion | cut -d':' -f2 | xargs)"
 else
-  CC_VER="$(gcc --version | head -n1)"
-  OS_VER="$(grep PRETTY_NAME /etc/os-release | cut -d'=' -f2- | tr -d '"')"
+  COMPILER_VER="$(gcc --version | head -n1)"
+  source /etc/os-release
+  OS_VER="${PRETTY_NAME}"
 fi
 
-NVCC_VER="$(nvcc --version | tail -n1)"
-
-if [[ -x "$(which powershell.exe)" ]]; then
+if [[ -x "$(command -v powershell.exe)" ]]; then
   OS_VER="$(powershell -Command "[System.Environment]::OSVersion.VersionString")"
 fi
 
-echo -e "CC_VER=$CC_VER\nNVCC_VER=$NVCC_VER\nOS_VER=${OS_VER}" | tee -a $0.out
+NVCC_VER="$(nvcc --version | tail -n1 | sed -E 's/^Build //')"
+
+echo -e "COMPILER_VER=$COMPILER_VER\nNVCC_VER=$NVCC_VER\nOS_VER=${OS_VER}" | tee -a $0.out
